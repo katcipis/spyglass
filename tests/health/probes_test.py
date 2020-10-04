31e0a0c1bff3e07be6b8cebc6697486faab2e1f3
Tiago Katcipis <tiagokatcipis@gmail.com>
2020-10-04 21:34:28 +0200

    Add test list for later

--- tests/health/probes_test.py
+++ tests/health/probes_test.py
@@ -3,6 +3,14 @@ import pytest
 from health.probes import http_probe
 
 
+# Tests
+# - Check response time
+# - 4XX responses
+# - 5XX responses
+# - Regex matching (success/failure)
+# - connection timeout
+# - DNS failure
+
 @pytest.mark.asyncio
 async def test_http_probe_success(httpx_mock):
     url = "http://test_http_probe_success"
