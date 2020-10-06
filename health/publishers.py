589874daf3e870579afeecb177efd306c833f64c
Tiago Katcipis <tiagokatcipis@gmail.com>
2020-10-07 01:15:57 +0200

    Fix lint issues

--- health/publishers.py
+++ health/publishers.py
@@ -2,20 +2,23 @@ import json
 
 from aiokafka import AIOKafkaProducer
 from aiokafka.helpers import create_ssl_context
+from aiokafka.errors import KafkaError, KafkaTimeoutError
+
 
 class KafkaPublisher:
     "Publishes health check status on Kafka"
 
     def __init__(
-        self, uri, cafile, certfile, keyfile, topic="spyglass.health.status"):
+      self, uri, cafile, certfile, keyfile, topic="spyglass.health.status"):
+
         context = create_ssl_context(
             cafile=cafile,
             certfile=certfile,
             keyfile=keyfile,
         )
-        # From: https://aiokafka.readthedocs.io/en/stable/producer.html#idempotent-produce
         # Seems like the idempotency and stronger guarantees are desirable if
-        # it supports the throughput. Would start with that and see how it scales.
+        # it supports the throughput.
+        # Would start with that and see how it scales.
         self.__topic = topic
         self.__producer = AIOKafkaProducer(
             bootstrap_servers=uri,
@@ -31,7 +34,6 @@ class KafkaPublisher:
         await self.__producer.stop()
 
     async def publish(self, url, status):
-        # TODO: add error handling: https://aiokafka.readthedocs.io/en/stable/api.html#error-handling
         publish_data = {
             "url": url,
             "status": {
@@ -48,4 +50,9 @@ class KafkaPublisher:
             }
 
         serialized = json.dumps(publish_data).encode()
-        await self.__producer.send_and_wait(self.__topic, serialized)
+        try:
+            await self.__producer.send_and_wait(self.__topic, serialized)
+        except KafkaTimeoutError:
+            print("TODO LOG")
+        except KafkaError as err:
+            print("TODO LOG", err)
